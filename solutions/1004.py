e5116a3937489735e5133adea3c28e60aa460711
Jounghun Kong <kjhun1208@khu.ac.kr>
2022-07-28 15:10:17 +0900

    Add solution

---
+++ solutions/1004.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    x1, y1, x2, y2 = map(int, input().split())
+    n = int(input())
+    cnt = 0
+    for i in range(n):
+        x, y, r = map(int, input().split())
+        d1 = ((x1-x)**2 + (y1-y)**2)**0.5
+        d2 = ((x2-x)**2 + (y2-y)**2)**0.5
+        if d1 < r and d2 > r:
+            cnt += 1
+        elif d1 > r and d2 < r:
+            cnt += 1
+
+    print(cnt)
